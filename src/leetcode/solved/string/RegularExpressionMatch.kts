33d2277796cb2857aaade7352fd46751d9dcceb3
Ayat <a.rakhishev@gmail.com>
2020-12-14 19:52:44 +0600

    Leetcode: 10. Regular Expression Matching

---
+++ src/leetcode/solved/string/RegularExpressionMatch.kts
@@ -0,0 +1,40 @@
+package leetcode.solved.string
+
+//10. Regular Expression Matching
+fun isMatch(s: String, p: String): Boolean {
+    if (s.isEmpty() && p.isEmpty())
+        return true
+    return check(0, 0, s, p)
+}
+
+fun check(indexS: Int, indexP: Int, s: String, p: String): Boolean {
+
+    if (indexP == p.lastIndex + 1 && indexS == s.lastIndex + 1)
+        return true
+    if (indexP > p.lastIndex && indexS <= s.lastIndex)
+        return false
+
+    if (indexP == p.lastIndex || p[indexP + 1] != '*') {
+
+        if(indexS>s.lastIndex)
+            return false
+        if (p[indexP] == '.' || s[indexS] == p[indexP]) {
+            if (indexP == p.lastIndex && indexS == s.lastIndex)
+                return true
+            else
+                return check(1 + indexS, 1 + indexP, s, p)
+        } else
+            return false
+
+    } else {
+        return check(indexS, indexP + 2, s, p) || //Zero occurrences
+                (if ( indexS <= s.lastIndex && (p[indexP] == '.' || s[indexS] == p[indexP])) {
+                    check(indexS + 1, indexP, s, p)// one or more
+                } else false) // count by one occurrence and check
+    }
+
+}
+
+fun main() {
+    print(isMatch("a", "ab*a"))
+}
