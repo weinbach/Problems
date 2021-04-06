b7e30f156c7ef7a244ad7b2308ceaf34fd78dc1b
Ayat <a.rakhishev@gmail.com>
2021-04-06 20:36:45 +0600

    inner minor changes

--- src/leetcode/solved/linkedList/CopyListWithRandomPointer.kt
+++ src/leetcode/solved/linkedList/CopyListWithRandomPointer.kt
@@ -3,6 +3,17 @@ package leetcode.solved.linkedList
 // 138. Copy List with Random Pointer
 // Runtime: 184 ms, faster than 43.92%
 // Memory Usage: 35.6 MB, less than 82.75%
+internal class Node(`val`: Int) {
+    var `val`: Int
+    var prev: Node? = null
+    var next: Node? = null
+    var child: Node? = null
+    var random: Node? = null
+
+    init {
+        this.`val` = `val`
+    }
+}
 private fun copyRandomList(node: Node?): Node? {
     if (node == null)
         return null
